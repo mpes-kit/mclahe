3fef10fa49a61a09c796e83964a8577caf2f7217
rettigl <l.rettig@web.de>
2025-05-01 01:02:05 +0200

    lint

--- mclahe/utils.py
+++ mclahe/utils.py
@@ -1,11 +1,9 @@
 #! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
-
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import gen_math_ops
+
+tf.disable_v2_behavior()
 
 
 def tf_batch_gather(params, indices, axis, name=None):
@@ -17,7 +15,8 @@ def tf_batch_gather(params, indices, axis, name=None):
     This operation assumes that the leading dimensions of `indices` are dense,
     and the gathers on the axis corresponding to the last dimension of `indices`.
     More concretely it computes:
-    `result[i1, ..., in, j1, ..., jm, k1, ...., kl] = params[i1, ..., in, indices[i1, ..., in, j1, ..., jm], k1, ..., kl]`
+    `result[i1, ..., in, j1, ..., jm, k1, ...., kl] =
+              params[i1, ..., in, indices[i1, ..., in, j1, ..., jm], k1, ..., kl]`
     Therefore `params` should be a Tensor of shape [A1, ..., AN, C0, B1, ..., BM],
     `indices` should be a Tensor of shape [A1, ..., AN, C1, ..., CK] and `result` will be
     a Tensor of size `[A1, ..., AN, C1, ..., CK, B1, ..., BM]`.
@@ -46,8 +45,7 @@ def tf_batch_gather(params, indices, axis, name=None):
 
         ndims = indices.shape.ndims
         if ndims is None:
-            raise ValueError("batch_gather does not allow indices with unknown "
-                             "shape.")
+            raise ValueError("batch_gather does not allow indices with unknown shape.")
         batch_indices = indices
         indices_dtype = indices.dtype.base_dtype
         accum_dim_value = tf.ones((), dtype=indices_dtype)
@@ -60,23 +58,23 @@ def tf_batch_gather(params, indices, axis, name=None):
             step = tf.ones((), dtype=indices_dtype)
             dim_indices = gen_math_ops._range(start, dim_value, step)
             dim_indices *= accum_dim_value
-            dim_shape = tf.stack([1] * (dim - 1) + [dim_value] + [1] * (ndims - dim),
-                                 axis=0)
+            dim_shape = tf.stack([1] * (dim - 1) + [dim_value] + [1] * (ndims - dim), axis=0)
             batch_indices += tf.reshape(dim_indices, dim_shape)
 
-        flat_inner_shape_indices = gen_math_ops.prod(indices_shape[:(axis + 1)], [0], False)
-        flat_indices = tf.reshape(batch_indices, tf.concat([[flat_inner_shape_indices],
-                                                            indices_shape[(axis + 1):]], axis=0))
-        outer_shape = params_shape[(axis + 1):]
-        flat_inner_shape_params = gen_math_ops.prod(params_shape[:(axis + 1)], [0], False)
+        flat_inner_shape_indices = gen_math_ops.prod(indices_shape[: (axis + 1)], [0], False)
+        flat_indices = tf.reshape(
+            batch_indices,
+            tf.concat([[flat_inner_shape_indices], indices_shape[(axis + 1) :]], axis=0),
+        )
+        outer_shape = params_shape[(axis + 1) :]
+        flat_inner_shape_params = gen_math_ops.prod(params_shape[: (axis + 1)], [0], False)
 
         flat_params = tf.reshape(params, tf.concat([[flat_inner_shape_params], outer_shape], axis=0))
         flat_result = tf.gather(flat_params, flat_indices)
         result = tf.reshape(flat_result, tf.concat([indices_shape, outer_shape], axis=0))
-        final_shape = indices.get_shape()[:axis].merge_with(
-                params.get_shape()[:axis])
+        final_shape = indices.get_shape()[:axis].merge_with(params.get_shape()[:axis])
         final_shape = final_shape.concatenate(indices.get_shape()[axis:])
-        final_shape = final_shape.concatenate(params.get_shape()[(axis + 1):])
+        final_shape = final_shape.concatenate(params.get_shape()[(axis + 1) :])
         result.set_shape(final_shape)
         return result
 
@@ -101,8 +99,12 @@ def tf_batch_histogram(values, value_range, axis, nbins=100, dtype=tf.int32, use
 
     if use_map:
         values_reshaped = tf.reshape(values, tf.concat([[num_batch], rest_dim], 0))
-        hist = tf.map_fn(lambda x: tf.histogram_fixed_width(x, value_range, nbins=nbins, dtype=dtype), values_reshaped,
-                         dtype=dtype, parallel_iterations=64)
+        hist = tf.map_fn(
+            lambda x: tf.histogram_fixed_width(x, value_range, nbins=nbins, dtype=dtype),
+            values_reshaped,
+            dtype=dtype,
+            parallel_iterations=64,
+        )
     else:
         # Normalize
         values_float = tf.cast(values, tf.float32)
@@ -114,11 +116,17 @@ def tf_batch_histogram(values, value_range, axis, nbins=100, dtype=tf.int32, use
         values_clip2 = tf.minimum(values_clip1, 1.0 - 0.5 / tf.cast(nbins, tf.float32))
 
         # Shift values
-        values_shift = values_clip2 + tf.reshape(tf.range(tf.cast(num_batch, tf.float32), dtype=tf.float32),
-                                                 tf.concat([batch_dim, tf.ones(tf.size(rest_dim), tf.int32)], 0))
+        values_shift = values_clip2 + tf.reshape(
+            tf.range(tf.cast(num_batch, tf.float32), dtype=tf.float32),
+            tf.concat([batch_dim, tf.ones(tf.size(rest_dim), tf.int32)], 0),
+        )
 
         # Get histogram
-        hist = tf.histogram_fixed_width(values_shift, [0., tf.cast(num_batch, tf.float32)], nbins=num_batch * nbins,
-                                        dtype=dtype)
+        hist = tf.histogram_fixed_width(
+            values_shift,
+            [0.0, tf.cast(num_batch, tf.float32)],
+            nbins=num_batch * nbins,
+            dtype=dtype,
+        )
 
     return tf.reshape(hist, tf.concat([batch_dim, [nbins]], 0))
